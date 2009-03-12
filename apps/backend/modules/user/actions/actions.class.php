9a8f58cd1c34046eb20b8867b900f58d49067b13
Bernard Grymonpon <bernard@openminds.be>
2009-03-12 12:54:11 +0100

    Added the generated new directories for backend::user

---
+++ apps/backend/modules/user/actions/actions.class.php
@@ -0,0 +1,16 @@
+<?php
+
+require_once dirname(__FILE__).'/../lib/userGeneratorConfiguration.class.php';
+require_once dirname(__FILE__).'/../lib/userGeneratorHelper.class.php';
+
+/**
+ * user actions.
+ *
+ * @package    uas
+ * @subpackage user
+ * @author     Your name here
+ * @version    SVN: $Id: actions.class.php 12474 2008-10-31 10:41:27Z fabien $
+ */
+class userActions extends autoUserActions
+{
+}
---
+++ apps/backend/modules/user/lib/userGeneratorHelper.class.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * user module helper.
+ *
+ * @package    uas
+ * @subpackage user
+ * @author     Your name here
+ * @version    SVN: $Id: helper.php 12474 2008-10-31 10:41:27Z fabien $
+ */
+class userGeneratorHelper extends BaseUserGeneratorHelper
+{
+}
