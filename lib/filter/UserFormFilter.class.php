8a0831984e6d12feb86ac72ade8e2606b8744491
Bernard Grymonpon <bernard@openminds.be>
2009-03-12 18:28:36 +0100

    Added the "none" option to the status filter list

--- lib/filter/UserFormFilter.class.php
+++ lib/filter/UserFormFilter.class.php
@@ -1,29 +1,29 @@
 <?php
 
 /**
- * User filter form.
- *
- * @package    uas
- * @subpackage filter
- * @author     Your name here
- * @version    SVN: $Id: sfPropelFormFilterTemplate.php 11675 2008-09-19 15:21:38Z fabien $
- */
+	* User filter form.
+	*
+	* @package    uas
+	* @subpackage filter
+	* @author     Your name here
+	* @version    SVN: $Id: sfPropelFormFilterTemplate.php 11675 2008-09-19 15:21:38Z fabien $
+	*/
 class UserFormFilter extends BaseUserFormFilter
 {
 
-  public function configure()
-  {
-	$this->setWidget('status', new sfWidgetFormChoice(array(
-		'choices' => UserPeer::$status_types
-		))
-	);
+	public function configure()
+	{
+		$this->setWidget('status', new sfWidgetFormChoice(array(
+			'choices' => array_merge(array('none' => ''), UserPeer::$status_types)
+			))
+			);
 
-	$this->setValidator('status', new sfValidatorChoice(array(
-		'required' => false, 
-		'choices' => array_keys(UserPeer::$status_types)
-		))
-	);
-  }
+		$this->setValidator('status', new sfValidatorChoice(array(
+			'required' => false, 
+			'choices' => array_merge(array('none'), array_keys(UserPeer::$status_types))
+			))
+			);
+	}
 
 	public function getFields()
 	{
@@ -31,4 +31,10 @@ class UserFormFilter extends BaseUserFormFilter
 			'status' => 'ForeignKey'
 			));
 	}
+
+	public function convertStatusValue($value)
+	  {
+	    return $value != 'none' ? $value : false;
+	  }
+
 }
