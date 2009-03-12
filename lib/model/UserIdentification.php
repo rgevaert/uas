5861f9f8940482ec379c970d2381bc69d01108c0
Eyuel Gessese <eyuel.gessese@ju.edu.et>
2009-03-12 17:18:01 +0100

    email alias: Removed the help text,user identification:  removed items and made it look a bit readable 	removed the help text from email alias 	added help text to new module 	added help text to add module 	added new function to the UserIdentification Model

--- lib/model/UserIdentification.php
+++ lib/model/UserIdentification.php
@@ -2,4 +2,11 @@
 
 class UserIdentification extends BaseUserIdentification
 {
+        public function getUserFullname()
+        {
+                $userid = $this->getUserId();
+                // Serves as an intermediary between the users and tbe databbase
+                $user = UserPeer::retrieveByPk($userid);
+                return $user->__toString();
+        }
 }
