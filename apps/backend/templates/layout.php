2d706b71e87273d0cb6c426962e601403aef1842
Bernard Grymonpon <bernard@openminds.be>
2009-03-12 16:09:07 +0100

    Redid the layout and css - might need some love in the future

--- apps/backend/templates/layout.php
+++ apps/backend/templates/layout.php
@@ -1,12 +1,38 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
-  <head>
-    <?php include_http_metas() ?>
-    <?php include_metas() ?>
-    <?php include_title() ?>
-    <link rel="shortcut icon" href="/favicon.ico" />
-  </head>
-  <body>
-    <?php echo $sf_content ?>
-  </body>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>User Administration System</title>
+	<?php use_stylesheet('admin.css') ?>
+   <?php include_javascripts() ?>
+   <?php include_stylesheets() ?></head>
+<body>
+<div id="header">
+	<div id="logo">
+		<h1><a href="#">UAS</a></h1>
+		<p>User Administration System</p>
+	</div>
+	<!-- end #logo -->
+	<div id="menu">
+		<ul>
+			      <li class="first"><?php echo link_to('Users', '@user') ?></li>
+		          <li><?php echo link_to('User Identifications', '@user_identification') ?></li>
+		          <li><?php echo link_to('Email aliases', '@email_alias') ?></li>
+		          <li><?php echo link_to('Domains', '@domainname') ?></li>
+		</ul>
+	</div>
+	<!-- end #menu -->
+</div>
+<!-- end #header -->
+<div id="page">
+	<div id="content">
+        <?php echo $sf_content ?>
+	</div>
+	<!-- end #content -->
+
+</div>
+<!-- end #page -->
+
+</body>
 </html>
+
