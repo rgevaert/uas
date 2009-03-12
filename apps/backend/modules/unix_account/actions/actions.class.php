a45e910f51cdc15015833a2feec0843e3ab8a6b0
Bernard Grymonpon <bernard@openminds.be>
2009-03-12 16:30:32 +0100

    Generated the backend module for UnixAccounts

---
+++ apps/backend/modules/unix_account/actions/actions.class.php
@@ -0,0 +1,16 @@
+<?php
+
+require_once dirname(__FILE__).'/../lib/unix_accountGeneratorConfiguration.class.php';
+require_once dirname(__FILE__).'/../lib/unix_accountGeneratorHelper.class.php';
+
+/**
+ * unix_account actions.
+ *
+ * @package    uas
+ * @subpackage unix_account
+ * @author     Your name here
+ * @version    SVN: $Id: actions.class.php 12474 2008-10-31 10:41:27Z fabien $
+ */
+class unix_accountActions extends autoUnix_accountActions
+{
+}
---
+++ test/functional/backend/unix_accountActionsTest.php
@@ -0,0 +1,19 @@
+<?php
+
+include(dirname(__FILE__).'/../../bootstrap/functional.php');
+
+$browser = new sfTestFunctional(new sfBrowser());
+
+$browser->
+  get('/unix_account/index')->
+
+  with('request')->begin()->
+    isParameter('module', 'unix_account')->
+    isParameter('action', 'index')->
+  end()->
+
+  with('response')->begin()->
+    isStatusCode(200)->
+    checkElement('body', '!/This is a temporary page/')->
+  end()
+;
