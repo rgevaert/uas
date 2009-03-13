6cae363e0a66a5e93dcee7b7e49231393351fbd3
Rudy Gevaert <rudy.gevaert@ugent.be>
2009-03-13 10:49:32 +0100

    Grouped the fields together and redid the email alias list as a stacked layout

--- lib/model/EmailAlias.php
+++ lib/model/EmailAlias.php
@@ -2,4 +2,13 @@
 
 class EmailAlias extends BaseEmailAlias
 {
+    public function __toString()
+    {
+        return $this->getLocalPart() . '@' . $this->getDomainName();
+    }
+    
+    public function getEmailAlias()
+    {
+        return $this->__toString();
+    }
 }
