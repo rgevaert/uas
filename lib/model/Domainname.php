c39e48428639d2da223b9c12b4508b51b910654a
Bernard Grymonpon <bernard@openminds.be>
2009-03-13 10:27:05 +0100

    Re-inserted the Domainname::__toString 	reverting the removal in a3a5ee3d8e6a0ccfd3bed78d493f3f7fef132810

--- lib/model/Domainname.php
+++ lib/model/Domainname.php
@@ -7,9 +7,14 @@ class Domainname extends BaseDomainname
 		// select count(*) from user where domainname_id = $this->getId()
 		$criteria = new Criteria();
 		$criteria->add(UserPeer::DOMAINNAME_ID, $this->getId());
-		
+
 		// do the counting on the user model
 		return UserPeer::doCount($criteria);
+	}
+
+	public function __toString()
+	{
+		return $this->getName();
 	}
 
 }
