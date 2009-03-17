8a8de2508e642c3cd83289c6d4277630204f7d0f
Eyuel Gessese <eyuel.gessese@ju.edu.et>
2009-03-17 11:09:37 +0100

    Provide basic login form and protect the complete backend

---
+++ apps/backend/modules/session/actions/actions.class.php
@@ -0,0 +1,42 @@
+<?php
+
+/**
+ * session actions.
+ *
+ * @package    uas
+ * @subpackage session
+ * @author     Your name here
+ * @version    SVN: $Id: actions.class.php 12479 2008-10-31 10:54:40Z fabien $
+ */
+class sessionActions extends sfActions
+{
+ /**
+  * Executes index action
+  *
+  * @param sfRequest $request A request object
+  */
+  public function executeIndex(sfWebRequest $request)
+  {
+    //$this->forward('default', 'module');
+    $this->redirect('session/login');
+  }
+  public function executeLogin(sfWebRequest $request)
+  {
+    $username = $request->getParameter('username');
+    $password = $request->getParameter('password');
+    if($username == 'admin' AND $password == 'pa55w0rd'){
+        $this->getUser()->setAuthenticated(true);
+        $this->redirect('@user');
+    }
+    else{
+        $this->getUser()->setFlash('error','User Name / Password Do not match');
+    }
+    
+  }
+  public function executeLogout(sfWebRequest $request)
+  {
+    $this->getUser()->setAuthenticated(false);
+    
+  }
+
+}
---
+++ apps/backend/modules/session/templates/loginSuccess.php
@@ -0,0 +1,16 @@
+   // apps/frontend/templates/layout.php
+
+   <?php if ($sf_user->hasFlash('notice')): ?>
+     <div class="flash_notice"><?php echo $sf_user->getFlash('notice')
+   ?></div>
+   <?php endif; ?>
+   <?php if ($sf_user->hasFlash('error')): ?>
+     <div class="flash_error"><?php echo $sf_user->getFlash('error') ?></div>
+   <?php endif; ?>
+
+<h2> Please Login here </h2>
+<form>
+<label for='username'> User Name </label><input type='text' name= 'username' /><br />
+<label for='password'> Password </label><input type='text' name = 'password' /><br />
+<input type='submit' value = 'Login' />
+</form>
