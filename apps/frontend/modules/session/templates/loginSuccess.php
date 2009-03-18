ea96d55406ee9729079f7fd079e8f20cb37d46d4
Habtom Kahsay <habtom222@yahoo.com>
2009-03-18 16:41:30 +0100

    Removed a link duplicate

--- apps/frontend/modules/session/templates/loginSuccess.php
+++ apps/frontend/modules/session/templates/loginSuccess.php
@@ -1,12 +1,10 @@
-<h1>Please Login Here</h1>
+<h1>Please login</h1>
 <form>
 	Username: <input name="login" /><br/>
 	Password: <input type="password" name="password" /><br/>
 	<input type="submit" value="login"><br/>
 	<br/>
-	<a href=''>Registration </a>
 </form>
-
 <p>If you do not have an account, you can <a href="<?php echo url_for('register/new') ?>">sign up</a>.</p>
 
 
