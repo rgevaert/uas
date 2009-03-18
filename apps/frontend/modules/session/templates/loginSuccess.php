26063dfe0cd8df8f8279f9d99c5ba63991e8aa5b
Habtom Kahsay <habtom222@yahoo.com>
2009-03-18 16:36:04 +0100

    Add minor refinements to login functionality with password inputs and setflash

--- apps/frontend/modules/session/templates/loginSuccess.php
+++ apps/frontend/modules/session/templates/loginSuccess.php
@@ -1,6 +1,8 @@
-<h1>Please login</h1>
+<h1>Please Login Here</h1>
 <form>
-	Username: <input name="login" /><br />
-	Password: <input name="password" /><br />
-	<input type="submit" value="login">
+	Username: <input name="login" /><br/>
+	Password: <input type="password" name="password" /><br/>
+	<input type="submit" value="login"><br/>
+	<br/>
+	<a href=''>Registration </a>
 </form>
