48846983fe79b83dbd8444033c98b23051045aab
Eyuel Gessese <eyuel.gessese@ju.edu.et>
2009-03-27 09:31:20 +0100

    password strength validator

--- lib/form/ChangePasswordForm.php
+++ lib/form/ChangePasswordForm.php
@@ -18,14 +18,23 @@ class ChangePasswordForm extends sfForm
     ));
     //Validators
     $this->validatorSchema['password'] = new sfValidatorString(array('required' => true));
-    $this->validatorSchema['new_password'] = new sfValidatorString(array('required' => true, 'min_length' => 8, 'max_length' => 8));
-    $this->validatorSchema['confirm_new_password'] = new sfValidatorString(array('required' => true, 'min_length' => 8, 'max_length' => 8));
-    $this->validatorSchema->setPostValidator(
-    new sfValidatorSchemaCompare('new_password', '==', 'confirm_new_password',
-    array('throw_global_error' => true),
-    array('invalid' => 'Password do not match.')
-    )
+
+    $this->validatorSchema['new_password'] = new sfValidatorAnd(array(
+    		new sfValidatorString(array('required' => true)),
+    		new uasValidatorPasswordIsStrong()
+		), 
+        array(), array('invalid' => 'Your new password is not strong enough!', 'required' => 'Please fill in you new password.')
     );
+
+    $this->validatorSchema['confirm_new_password'] = new sfValidatorString(array('required' => true, 'min_length' => 8));
+
+    $this->validatorSchema->setPreValidator(
+        new sfValidatorSchemaCompare('new_password', '==', 'confirm_new_password',
+            array('throw_global_error' => true),
+         array('invalid' => 'Your confirmed password does not match.')
+        )
+    );
+
     
     $this->widgetSchema->setNameFormat('changepassword[%s]');  
   }
--- lib/uasValidators.php
+++ lib/uasValidators.php
@@ -39,6 +39,39 @@ class uasValidatorPasswordIsCorrect extends sfValidatorBase
 
     return $value;
   }
+
+}
+class uasValidatorPasswordIsStrong extends sfValidatorBase
+{
+  protected function configure($options = array(), $messages = array())
+  {
+	$this->addOption('login');
+    parent::configure($options, $messages);
+  }
+
+  protected function doClean($value)
+  {
+	$password = $_POST['changepassword']['new_password'];
+    
+    if( preg_match_all('/[a-z]/', $password, $small_letter_match) > 1 &&
+        preg_match_all('/[0-9]/', $password, $number_match) > 1 &&
+        preg_match_all('/[&\$@#\)\(\[\]\?><!]/', $password, $special_char_match) > 1 &&
+        preg_match_all('/[A-Z]/', $password, $cap_letter_match) > 1  &&
+        strlen($password) > 7 )
+    {
+        return true;
+    }
+
+/*
+    if( preg_match_all('/[a-z]/', $password, $small_letter_match) < 2 ) die("$password small"); 
+    if( preg_match_all('/[0-9]/', $password, $number_match) < 2) die("$password number"); 
+    if( preg_match_all('/[&\$@#\)\(\[\]\?><!]/', $password, $special_char_match) < 2) die("$password char"); 
+    if( preg_match_all('/[A-Z]/', $password, $cap_letter_match) < 2 ) die("$password cap"); 
+    if( strlen($password) < 8 ) die("$password len");
+*/
+    return false;
+
+  }
 }
 
 ?>
