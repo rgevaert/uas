ae278ce8bf83ad765e654799773c6d9c29e61471
eyuel <ict@ict-ju.(none)>
2009-04-18 21:20:12 +0300

    Added a task that sends login name to users

--- lib/task/uasSendLoginNamesTask.class.php
+++ lib/task/uasSendLoginNamesTask.class.php
@@ -13,6 +13,10 @@ class uasSendLoginNamesTask extends sfBaseTask
       new sfCommandArgument('to', sfCommandArgument::OPTIONAL, 'TO'),
     ));    
 
+    $this->addArguments(array(
+      new sfCommandArgument('all', sfCommandArgument::OPTIONAL, 'ALL'),
+    ));        
+
     $this->addOptions(array(
       new sfCommandOption('application', null, sfCommandOption::PARAMETER_REQUIRED, 'The application name'),
       new sfCommandOption('env', null, sfCommandOption::PARAMETER_REQUIRED, 'The environment', 'dev'),
@@ -37,29 +41,51 @@ EOF;
     $databaseManager = new sfDatabaseManager($this->configuration);
     $connection = $databaseManager->getDatabase($options['connection'] ? $options['connection'] : null)->getConnection();
 
-     $to = $arguments['to'] ? $arguments['to'] : 'eyuel.gessese@ju.edu.et';
+    $to = $arguments['to'] ? $arguments['to'] : '';
     
     // TODO : get login, localparts names
 
-     foreach  (UserPeer::getEmailAccounts() as $user)
+     $subject = "Notification";
+     $headers = 'From: ICT <support@ju.edu.et>';
+     $message = <<<EOF
+Hello #FIRSTNAME# #FATHERSNAME#,
+
+We would like to inform you about the following.
+
+Your login name for the User Adminitration System is: #LOGIN# 
+
+Please go to http://mail.ju.edu.et/uas/ to use it.  The password to log in
+is the same as for the email system.
+
+Login in into the email system is still done with your email address.
+
+Thank you for your cooperation.  We hope you will enjoy the UAS.
+Expect us to expand its functionality in the future.
+
+The ICT Development office
+EOF;
+ 
+     $users = array();
+     if($to != '')
      {
-          if($user->getEmailAddress()==$to){
+               $users[] = UserPeer::getUserFromLogin($to);
                
-               $login = $user->getLogin($user->getEmailAddress())."\n"; 
-               $subject = "Notification";
-               $headers = 'From: ICT <support@ju.edu.et>' . "\r\n";
-               $message = "Hello there, your login is : $login";
-               if(mail($to, $subject, $message, $headers))
-               {
-                    echo "message sent";
-               }
-               else
-               {
-                    echo "message failed";
-               }
-          }
+     }
+     else
+     {
+               $users = UserPeer::getEmailAccounts();
+     }
+
+     foreach($users as $user)
+     {       
+          $login = $user->getLogin(); 
+          $search  = array("#LOGIN#", "#FIRSTNAME#", "#FATHERSNAME#", "#EMAILADDRESS#");
+          $replace = array($user->getLogin(), $user->getName(), $user->getFathersName(), $user->getEmailAddress());
+          if(!mail($user->getEmailAddress(), $subject, str_replace($search, $replace, $message), $headers))
+          {
+               echo "message failed";
+          }    
      }
     
-    // Send login names to each mail users
   }
 }
