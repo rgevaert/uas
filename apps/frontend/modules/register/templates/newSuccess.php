d324b7da5dbca154cb19467b2a5c4246f686d06d
Yemane <yemane@yemane-desktop.(none)>
2009-04-23 15:43:50 +0300

    Added a back link on register form

--- apps/frontend/modules/register/templates/newSuccess.php
+++ apps/frontend/modules/register/templates/newSuccess.php
@@ -3,3 +3,4 @@
 <p>Please fill in the form to register for an ICT account.</p>
 
 <?php include_partial('form', array('form' => $form)) ?>
+<a href="<?php echo url_for('session/login') ?>">Back to login page</a>
