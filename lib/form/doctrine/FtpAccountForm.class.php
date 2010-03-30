aeb08646a3a5955f1b5a155ae6b0c511ccd08588
Bernard Grymonpon <bernard@openminds.be>
2010-03-30 14:43:30 +0300

    ftpaccounts upgraded to doctrine

--- lib/form/doctrine/FtpAccountForm.class.php
+++ lib/form/doctrine/FtpAccountForm.class.php
@@ -12,13 +12,13 @@ class FtpAccountForm extends BaseFtpAccountForm
 {
   public function configure()
   {
+		unset($this['created_at'], $this['updated_at']);
+
         $this->validatorSchema['up_bandwidth'] = new sfValidatorInteger(array('min'=>0));        
         $this->validatorSchema['down_bandwidth'] = new sfValidatorInteger(array('min'=>0));       
         $this->validatorSchema['quota_files'] = new sfValidatorNumber(array('min'=>0)); 
         $this->validatorSchema['quota_size'] = new sfValidatorNumber(array('min'=>0)); 
-        $this->validatorSchema['ip_access'] = new sfValidatorRegex(
-                array('pattern'=>'/^\*$/')
-              );
+        $this->validatorSchema['ip_access'] = new sfValidatorRegex(array('pattern'=>'/^\*$/'));
   }
                  
 }
