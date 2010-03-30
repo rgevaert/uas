a352c7f73a10ff2a6a153c2a200d3bd085cc7cc4
Bernard Grymonpon <bernard@openminds.be>
2010-03-30 14:46:15 +0300

    samba accounts upgraded to doctrine

--- lib/form/doctrine/SambaAccountForm.class.php
+++ lib/form/doctrine/SambaAccountForm.class.php
@@ -1,19 +1,17 @@
 <?php
 /**
- * SambaAccount form.
- *
- * @package    uas
- * @subpackage form
- * @author     Your name here
- * @version    SVN: $Id: sfPropelFormTemplate.php 10377 2008-07-21 07:10:32Z dwhittle $
- */
+* SambaAccount form.
+*
+* @package    uas
+* @subpackage form
+* @author     Your name here
+* @version    SVN: $Id: sfPropelFormTemplate.php 10377 2008-07-21 07:10:32Z dwhittle $
+*/
 class SambaAccountForm extends BaseSambaAccountForm
 {
-  public function configure()
-  {
-  $this->validatorSchema['hostname'] = new sfValidatorRegex(array
-	     ('pattern'=>('/^(?:[-a-z0-9]+\.)+[a-z]{2,}$/')));
-  }
-}
+	public function configure(){
+		unset($this['created_at'], $this['updated_at']);
 
-?>
+		$this->validatorSchema['hostname'] = new sfValidatorRegex(array('pattern'=>('/^(?:[-a-z0-9]+\.)+[a-z]{2,}$/')));
+	}
+}
